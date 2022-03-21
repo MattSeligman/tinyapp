1c76441f14f93290b0e21911b4e8856981ae48d1
MattSeligman <matt@modelitup.com>
2022-03-21 15:15:06 -0700

    Added comments

--- express_server.js
+++ express_server.js
@@ -1,20 +1,25 @@
+// Imports & Port Configuration
 const express = require('express');
 const app = express();
 const PORT = 8080; // default port 8080
 
+// Database of shortUrl : URL Redirection Path
 const urlDatabase = {
   'b2xVn2': 'http://www.lighthouselabs.ca',
   '9sm5xK': 'http://www.google.com'
 };
 
+// Route (Homepage)
 app.get('/', (req, res) => {
   res.send('Hello!');
 });
 
+// Route (/urls.json Viewable for testing purposes)
 app.get("/urls.json", (req, res) => {
     res.json(urlDatabase);
   });
 
+// Starts the Server & Listens on PORT (Console Log on success)
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
 });
