26a6a8871084918d3d32a07bdc1895ea27141882
MattSeligman <matt@modelitup.com>
2022-03-25 18:22:13 -0700

    Moved the head partial into the pages to control the Title, and Meta data easier

--- test/helpersTest.js
+++ test/helpersTest.js
@@ -19,21 +19,21 @@ describe('getUserByEmail', function() {
   it('`user@example.com` should return `userRandomID`', function() {
     const user = getUserByEmail("user@example.com", testUsers);
     const expectedUserID = "userRandomID";
-    assert.deepEqual(user.id, expectedUserID)
+    assert.deepEqual(user.id, expectedUserID);
   });
   it('`user2@example` should return `user2RandomID`', function() {
     const user = getUserByEmail("user2@example.com", testUsers);
     const expectedUserID = "user2RandomID";
-    assert.deepEqual(user.id, expectedUserID)
+    assert.deepEqual(user.id, expectedUserID);
   });
   it('`notExisting` should not return a user with valid email', function() {
     const user = getUserByEmail("user@example.com", testUsers);
     const expectedUserID = "notExisting";
-    assert.notDeepEqual(user.id, expectedUserID)
+    assert.notDeepEqual(user.id, expectedUserID);
   });
   it('`undefined` should not return a user with valid email', function() {
     const user = getUserByEmail(undefined, testUsers);
     const expectedUserID = undefined;
-    assert.deepEqual(user.id, expectedUserID)
+    assert.deepEqual(user.id, expectedUserID);
   });
 });
